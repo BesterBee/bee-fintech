ae057d9429266e5e7066cae5749de63719555132
bester.sibanda <bester.sibanda@cassavasmartech.co.zw>
2026-06-03 16:07:20 +0200

    we now working on login, dont ask why i started with auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Register } from './features/auth/register/register';
+import { Login } from './features/auth/login/login';
+import { Dashboard } from './features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/register', pathMatch: 'full'},
+  {path: 'register', component: Register},
+  {path: 'login', component: Login},
+  {path: 'dashboard', component: Dashboard},
+];
+
